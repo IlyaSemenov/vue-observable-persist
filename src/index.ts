7fad7ec6385b3a4f34be5762ba7dcbb4275a5c48
Ilya Semenov <semenov@inetss.com>
2020-07-02 20:47:01 +0700

    fix: support environments with no localStorage

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import { merge, pick } from "lodash"
 import Vue from "vue"
 
 interface Options {
-	storage: Pick<Storage, "getItem" | "setItem">
+	storage?: Pick<Storage, "getItem" | "setItem">
 	key: string
 	fields?: string[]
 	serialize: (data: any) => any
@@ -10,7 +10,6 @@ interface Options {
 }
 
 const defaults: Options = {
-	storage: localStorage,
 	key: "store",
 	serialize: JSON.stringify,
 	deserialize: JSON.parse,
@@ -19,14 +18,15 @@ const defaults: Options = {
 export default function persist<T>(store: T, opts?: Partial<Options>): T {
 	const options: Options = { ...defaults, ...opts }
 
+	// In some environments there is no localStorage at all.
+	// If we provided localStorage in defaults, it would crash.
+	const storage = options.storage || localStorage
+
 	const filter_data = (data: T) =>
 		options.fields ? pick(data, options.fields) : data
 
 	try {
-		merge(
-			store,
-			filter_data(options.deserialize(options.storage.getItem(options.key))),
-		)
+		merge(store, filter_data(options.deserialize(storage.getItem(options.key))))
 	} catch (err) {}
 
 	// Support Vue objects and plain observable objects
@@ -39,11 +39,7 @@ export default function persist<T>(store: T, opts?: Partial<Options>): T {
 	})
 	watcher.$watch(
 		"data",
-		data =>
-			options.storage.setItem(
-				options.key,
-				options.serialize(filter_data(data)),
-			),
+		data => storage.setItem(options.key, options.serialize(filter_data(data))),
 		{ deep: true },
 	)
 
